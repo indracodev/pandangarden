ba9754ff96fdcafab35c70205b017ceeba00a30a
khunztul <nanda.messi10@gmail.com>
2022-07-08 10:32:34 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::CustomerResource('/customer', App\Http\Controllers\CustomerController::class);
+Route::ApiResource('/customers', App\Http\Controllers\CustomerController::class);
